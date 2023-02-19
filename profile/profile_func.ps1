2d2bdcc34b187ae69b533743f2ed07f8a8093801
Sanjeev Stephan <samstephan164@gmail.com>
2023-02-19 10:06:55 +0530

    Added few functions TODO() | launch()

--- profile/profile_func.ps1
+++ profile/profile_func.ps1
@@ -3,7 +3,7 @@
 #. "$HOME\Documents\PowerShell\profile_include.ps1"
 #-------------------- Functions {Pass-Argument}----------------
 function helpnow(){
-
+#figlet "Terminal Help "
 #$func_name.GetEnumerator() | Format-Table Name, Value -AutoSize
 
 $func_name.GetEnumerator() | Format-Table @{label="Command"; expression={$_.Key}}, @{label="Syntax"; expression={$_.Value}} -AutoSize
@@ -27,7 +27,7 @@ function goto($dir_name) {
             "py" 
             {
                 figlet("PyScript")
-                cd $paths[$dir_name]
+                cd $paths["pyscript"]
                 dir
               
             }
@@ -44,29 +44,55 @@ function goto($dir_name) {
             }
             Default 
             {
-                Write-Host "You entered the directory[$dir_name] that is not on the profile dictionary"
+                
+                Write-Host "[Invalid] You entered the directory[$dir_name] that is not on the profile dictionary"
+                figlet 'goto'
+                Write-Host "[Try] 'goto <directory-name> | with 'directory name from below' list"
+                $paths.GetEnumerator() | Format-Table @{label="Directory Name"; expression={$_.Key}}, @{label="Path"; expression={$_.Value}} -AutoSize
+
             }
         }
 
 }
-function run($script_name) {
+function run($script_name_n_url) {
     
-    Switch($script_name)
+    Switch($script_name_n_url)
     {
 
         "pymath"
         {
-            Write-Host "Pymath is running"
+            Write-Host "PyMath executed Successfully"
+            cd $paths["pymath"]
+            & python  $script[$script_name_n_url]
         }
-
+        "pygame"
+        {
+           Write-Host "PyMath executed Successfully"
+           & python  $script[$script_name_n_url]
+        }
         Default
         {
-            Write-Host "Invalid Script Name | Retry "
+            Write-Host "[Invalid] Script Name Not Mentioned in the Dictionary "
+            figlet "run"
+            Write-Host "[Try] run <script_name> | 'script_name' from below list"
+            $script.GetEnumerator() | Format-Table @{label="Script Name"; expression={$_.Key}}, @{label="Path"; expression={$_.Value}} -AutoSize
+
         }
     }
 }
-function google($text_to_query) {
+function launch($app_name){
 
+    switch($app_name) {
+            "notepad" { Start-Process "notepad.exe"}
+            "snap"    { Start-Process "SnippingTool.exe"}
+            Default   { Start-Process $app_name}
+
+       }
+}
+function google() {
+   figlet "Google"
+  $google_search = $script["google_query"] # retrive the value from the script dictionary
+  .$google_search #execute the script
 }
 function greet($name,$age) {
     Write-Host "Hello, My Name is $name and i am $age year old"
@@ -99,3 +125,26 @@ Rename-Item $filename $file_to_be_renamed
 Write-Host $filename successfully renamed
 
 }
+function TODO($action){
+ # Display the TODO-List"
+   cat $profile_source\TODO
+
+    switch($action)
+    {
+        #open a text editor in nano-terminal-editor and save the todo-list
+        "edit" { Start-Process $profile_source\nano.exe $profile_source\TODO}
+        # Display the TODO-List"
+        "list" {cat $profile_source\TODO}
+        "exit" { exit }
+        Default {Write-Host "Want to Edit | Try -> TODO <edit | list | exit>" }
+    }
+
+}
+
+function listhash() {
+#Get-ChildItem | Select-Object LastWriteTime, Length, Name, @{Name="FileHash";Expression={(Get-FileHash -A SHA1 $_.FullName).Hash}}
+Get-ChildItem | Select-Object Name, @{Name="FileHash";Expression={(Get-FileHash $_.FullName).Hash}}
+}
+
+# Sample dictionry format-table
+#  $paths.GetEnumerator() | Format-Table @{label="Directory Name"; expression={$_.Key}}, @{label="Path"; expression={$_.Value}} -AutoSize
