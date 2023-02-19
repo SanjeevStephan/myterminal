a5783c5ce01c2aff6ff38ccde45204e6e8a0faa6
Sanjeev Stephan <samstephan164@gmail.com>
2023-02-19 10:07:45 +0530

    Made few changes to the the profile_path's dictionaries

--- profile/profile_path.ps1
+++ profile/profile_path.ps1
@@ -1,22 +1,36 @@
 $paths  = @{
-         "pyscript"        = "D:\pyscripts"
-         "terminal"        = "D:\terminal"
          "powershell"      =  "$HOME\Documents\PowerShell"
+         "terminal"        = "D:\terminal"
          "terminal_profile"= "D:\terminal\profile"
+         "pyscript"        = "D:\pyscripts"
+         "pymath"          = "D:\pyscripts\pymath"
           
           }
 $script = @{
             "figlet" = "D:\terminal\py\figlet.py"
+            "pymath" = "D:\pyscripts\pymath\pymath.py"
+            "pygame" = "D:\pyscripts\pygame\pygame.py"
+            "google_query" = "D:\terminal\taskarray\send-google-query.ps1"
     
            }
 $func_name = @{
+    "backup"                   = "backup directory $HOME/Documents/PowerShell"
+    "clip <filename|fileurl>"  = "paste the clipboard into the file"
+    "edit <filename>"          = "edit files in powershell_ise"
     "figlet <text_to_display>" = "display cool figlet ascii text " 
     "goto <directory_name>"    = "navigate to directory {py|terminal}"
+    "google"                   = "Send Query to the Google using brave browser"
+    "helpnow"                  = "Display this help menu"
+ #   "location"                 = "location of the scripts and directory in profile dictionary"
+    "nano <filename>"          = "open files in nano-terminal-editor"
     "run <script_name>"        = "execute script like {pygame|pymath}"
     "refresh"                  = "reload the Microsoft.PowerShell.Profile.ps1 script"
-    "location"                 = "location of the scripts and directory in profile dictionary"
-    "helpnow"                  = "Display this help menu"
-    "google <text_to_query>"   = "Send Query to the Google using brave browser"
+    "TODO"                     = "Open TODO List in Nano Editor"
+    "window"                   = "open file explorer of the current working directory"
+    
+
+    
+     
 
 
      }
