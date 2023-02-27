a04dcdbda403f8bde60b4cb997af0e09f9462239
Sanjeev Stephan <samstephan164@gmail.com>
2023-02-27 20:46:47 +0530

    send log text to python log script

--- code-snippets/log-display.bat
+++ code-snippets/log-display.bat
@@ -1,4 +1,6 @@
 @echo off
+python figlet.py --message "Log Display"
 python log.py --text "To Use python log.py --text 'Text to Display As Log'"
-python log.py --text "simple test text"
+python log.py --text "To Use python log.py --text ' .\log-display.bat text-to-display-here'
+python log.py --text "You Have Entered Text : %1"
 
