27c0f1457adbabb7e75c98f70636c6f1fb8c7bb6
Sanjeev Stephan <samstephan164@gmail.com>
2023-02-28 11:04:13 +0530

    added filter scripts

---
+++ code-snippets/syntax-filter.bat
@@ -0,0 +1,39 @@
+@echo off
+setlocal enabledelayedexpansion
+set scriptname="Syntax Filter"
+
+python figlet.py --message %scriptname%
+python line.py --range 75
+
+set extension=%1
+set substring=%2
+
+if "%extension%"=="" (
+    echo No argument passed.
+    REM echo Try 
+    pause
+    exit /b
+)
+
+echo Extension to look for first : %extension%
+echo Substring to Filter : %substring%
+
+python line.py --range 75
+
+REM Filter Every fles for Specific extension and substring
+for %%i in (*.%extension%) do (
+    set "filename=%%i"
+    if "!filename!"=="!filename:%substring%=!" (
+        rem do nothing
+    ) else (
+        set /a count+=1
+	echo [!count!] !filename!
+        REM echo !count!. !filename!
+	
+        set "file[!count!]=!filename!"
+	
+    )
+)
+
+echo "Total Files Found with name [%2]=> !count!"
+
