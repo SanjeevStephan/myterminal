e0e17f8cc5240644841001b89a4cac3f30cbf811
Sanjeev Stephan <samstephan164@gmail.com>
2023-02-28 19:42:30 +0530

    batch log files

--- matrix/log-display.bat
+++ matrix/log-display.bat
@@ -1,6 +1,6 @@
 @echo off
-python figlet.py --message "Log Display"
-python log.py --text "To Use python log.py --text 'Text to Display As Log'"
-python log.py --text "To Use python log.py --text ' .\log-display.bat text-to-display-here'
-python log.py --text "You Have Entered Text : %1"
+python neo\figlet.py --message "Log Display"
+python neo\log.py --text "To Use python log.py --text 'Text to Display As Log'"
+python neo\log.py --text "To Use python log.py --text ' .\log-display.bat text-to-display-here'
+python neo\log.py --text "You Have Entered Text : %1"
 
