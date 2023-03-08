70c263a150247534c9b097652ff30104a025af53
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-08 18:53:34 +0530

    JioFiber Network Keys Manager

---
+++ matrix/oracle/jiofiber_networks_keys/jiofiber_manager_config_v5_0.py
@@ -0,0 +1,16 @@
+
+json_filename = "saved_todo_list.json"
+
+script_name = "JioFiber Networks"
+
+
+column_header_1 = "Serial"
+column_header_2 = "SSID"
+column_header_3 = "Date"
+column_header_4 =  "Key"
+
+item_name = "SSID"
+
+
+
+manual_path = file_path = 'file://D:/terminal/.chatgpt_responses/JSON-TO-DO-MENU-MANAGER.mhtml'
---
+++ matrix/oracle/jiofiber_networks_keys/launch_manager_5.0.cmd
@@ -0,0 +1,1 @@
+python jiofiber_manager_menu_v5_0.py
