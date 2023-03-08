a1efa37b4b756e5380f0ac8221260a4da75c8fae
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-08 18:53:11 +0530

    updated json manager v5.0

---
+++ matrix/oracle/TEMPLATE_JSON_MANAGER/json_manager_config_v5_0.py
@@ -0,0 +1,16 @@
+
+json_filename = "saved_todo_list.json"
+
+script_name = "json to-do list"
+
+
+column_header_1 = "Serial"
+column_header_2 = "Title"
+column_header_3 = "Date"
+column_header_4 =  "Status"
+
+
+item_name = "item"
+
+
+manual_path = file_path = 'file://D:/terminal/.chatgpt_responses/JSON-TO-DO-MENU-MANAGER.mhtml'
