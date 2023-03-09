14280cd6565adbc7c7fa1ecabcad701eba5267fd
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-09 18:40:29 +0530

    terminal setup installer scripts

---
+++ setup_script.ps1
@@ -0,0 +1,8 @@
+$script = @{
+    "backup_profile"  = "D:\terminal\setup_task\backup-profile.ps1"
+    "restore_profile" = "D:\terminal\setup_task\restore-profile.ps1"
+    "install_python" = "D:\terminal\setup_tool\install_python.ps1"
+    "pip_installer"  = "D:\terminal\setup_pkg\pip_installer\install_python_dependencies.ps1"
+
+
+}
