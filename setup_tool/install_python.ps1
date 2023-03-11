799f099a7c624b66eb707a837f3799fa9b7647b7
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-11 06:28:54 +0530

    fixed the python installer path

--- setup_tool/install_python.ps1
+++ setup_tool/install_python.ps1
@@ -1,17 +1,27 @@
+#python-3.11.2-amd64.exe
+$python_home = "$HOME\AppData\Local\Programs\Python\"
 
-$python_home = "test-path $HOME\AppData\Local\Programs\Python\"
+
+$python_insall_path = "D:\terminal\setup_tool"
 $python_installer = "python-3.11.2-amd64.exe"
 function installpython() {
 
-    if (test-path $python_home) { Write-Output "Python is Already Installed" }
+    if (test-path $python_home) { 
+    $py_version = python --version
+    Write-Output "Python is Already Installed | Installed Version : $py_version" 
+    return "true"
+    }
     else {
     Write-Output "Python is not Installed"
     Write-Output "Installing $python_installer..."
     Write-Output "Please wait...Launching GUI Installer"
-    cd D:\terminal\tools
+    cd $python_insall_path
     .\python-3.11.2-amd64.exe
     #Start-Sleep -Seconds 5
+
+    return "false"
     }
+
 }
 
 function ifclosed($app_name) {
@@ -26,10 +36,24 @@ function ifclosed($app_name) {
 
 }
 
-installpython
-ifclosed($python_installer)
+
+function finalCheck() {
+
 Write-Output "Python Installer is closed Now"
 
+$choice = Read-Host "[INPUT] Have You Installed Python Correctly (y/n) : "
+if ($choice -eq "y") 
+{
+    Write-Output "Checking the Python Version now"
+    $py_version = python --version
+    if (!($py_version -eq "")) { Write-Output "Python Is Installed Correctly -> Python Version is : $py_version"}
+    else { Write-Output "Try Again" }
 
 
-#python-3.11.2-amd64.exe
+} else { Write-Output "Install Correctly and then restart the terminal setup" }
+
+}
+
+installpython
+
+if (installpython -eq "true") { finalCheck } else {  ifclosed($python_installer)}
