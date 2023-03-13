8e4de7050c058f942609587e92b2ebc4d1512987
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-13 09:40:51 +0530

    added 'alphabet_position' to scripts

--- profile/profile_path.ps1
+++ profile/profile_path.ps1
@@ -9,6 +9,7 @@ $paths  = @{
          "pymath"          = "D:\pyscripts\pymath"
          "project"         = "D:\terminal\project"
          "manual"          = "$HOME\Documents\PowerShell\.template"
+         "random_math"     = "D:\pyscripts\pymath\random_maths"
 
 
          }
@@ -19,6 +20,7 @@ $goto_paths  = @{
          "profile"         = "D:\terminal\profile"
          "py"              = "D:\pyscripts"
          "pymath"          = "D:\pyscripts\pymath"
+         "random_math"     = "D:\pyscripts\pymath\random_maths"
          "project"         = "D:\terminal\project"
          "manual"          = "$HOME\Documents\PowerShell\.template"
          "matrix"         = "D:\terminal\matrix"
@@ -31,6 +33,7 @@ $script = @{
             "figlet" = "D:\terminal\setup_pkg\figlet.py"
             "pymath" = "D:\pyscripts\pymath\pymath.py"
             "pygame" = "D:\pyscripts\pygame\pygame.py"
+            "random_math" = "D:\pyscripts\pymath\random_maths\pymath.bat"
             "check_internet" = "D:\terminal\taskarray\are-you-online.ps1"
             "google_query"  = "D:\terminal\taskarray\send-google-query.ps1"
             "quotesoftheday"= "D:\terminal\matrix\oracle\random_quotes_generator\quotes-of-the-day.ps1"
@@ -38,5 +41,9 @@ $script = @{
             "timetable_mwf" = "D:\terminal\matrix\oracle\timetable\launch-timetable-MWF.cmd"
             "timetable_tts" = "D:\terminal\matrix\oracle\timetable\launch-timetable-TTS.cmd"
             "vocabulary"    = "D:\terminal\matrix\oracle\vocabulary\launch-vocabulary.cmd"
+            "time_per_ques" = "D:\terminal\code-snippets\powershell_to_calculate_time_per_ques_to_answer.ps1"
+            "showip_status"   = "D:\terminal\py\args_prettytable_ip_status.py"
+            "alphabet_position" = "D:\pyscripts\pygame\launch_alphabet_position.cmd"
     
            }
+           #https://ss64.com/ps/
