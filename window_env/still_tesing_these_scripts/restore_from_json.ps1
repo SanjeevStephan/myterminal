328c51b14830b2bf6cd3f3b6691dbc19881014f7
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-14 05:32:19 +0530

    removed

--- window_env/still_tesing_these_scripts/restore_from_json.ps1
+++
@@ -1,7 +0,0 @@
-# Restore the environment variables from the JSON file
-$envJson = Get-Content -Raw -Path "json_env_variables.json"
-$envVariables = $envJson | ConvertFrom-Json
-foreach ($var in $envVariables) {
-    [Environment]::SetEnvironmentVariable($var.Name, $var.Value)
-}
-
