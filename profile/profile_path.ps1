47e8189bf07895e5de1a98d57a45686ae2a6d7b7
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-19 21:21:00 +0530

    added pico and mr_robot to goto_paths

--- profile/profile_path.ps1
+++ profile/profile_path.ps1
@@ -10,12 +10,16 @@ $paths  = @{
          "project"         = "D:\terminal\project"
          "manual"          = "$HOME\Documents\PowerShell\.template"
          "random_math"     = "D:\pyscripts\pymath\random_maths"
+         "study"           = "D:\study"
+         "alphabet_position" = "D:\study\learn_alphabet_positions"
+ 
+
 
 
          }
 
 $goto_paths  = @{
-         "pshell"          =  "$HOME\Documents\PowerShell"
+         "pshell"          = "$HOME\Documents\PowerShell"
          "terminal"        = "D:\terminal"
          "profile"         = "D:\terminal\profile"
          "py"              = "D:\pyscripts"
@@ -23,9 +27,12 @@ $goto_paths  = @{
          "random_math"     = "D:\pyscripts\pymath\random_maths"
          "project"         = "D:\terminal\project"
          "manual"          = "$HOME\Documents\PowerShell\.template"
-         "matrix"         = "D:\terminal\matrix"
-         "oracle"         = "D:\terminal\matrix\oracle"
+         "matrix"          = "D:\terminal\matrix"
+         "oracle"          = "D:\terminal\matrix\oracle"
          "codes"           = "D:\terminal\code-snippets"
+         "study"           = "D:\study"
+         "mr_robot"        = "D:\mr_robot"
+         "pico"            = "D:\mr_robot\pico_ducky"
           
           }
 
@@ -33,7 +40,7 @@ $script = @{
             "figlet" = "D:\terminal\setup_pkg\figlet.py"
             "pymath" = "D:\pyscripts\pymath\pymath.py"
             "pygame" = "D:\pyscripts\pygame\pygame.py"
-            "random_math" = "D:\pyscripts\pymath\random_maths\pymath.bat"
+            "random_math" = "D:\study\pymaths.bat"
             "check_internet" = "D:\terminal\taskarray\are-you-online.ps1"
             "google_query"  = "D:\terminal\taskarray\send-google-query.ps1"
             "quotesoftheday"= "D:\terminal\matrix\oracle\random_quotes_generator\quotes-of-the-day.ps1"
@@ -43,7 +50,9 @@ $script = @{
             "vocabulary"    = "D:\terminal\matrix\oracle\vocabulary\launch-vocabulary.cmd"
             "time_per_ques" = "D:\terminal\code-snippets\powershell_to_calculate_time_per_ques_to_answer.ps1"
             "showip_status"   = "D:\terminal\py\args_prettytable_ip_status.py"
-            "alphabet_position" = "D:\pyscripts\pygame\launch_alphabet_position.cmd"
+            "alphabet_position" = "D:\study\alphabet_position.bat"
+            "alphabet_reverse_position" = "D:\study\alphabet_reverse_position.bat"
+            "pico_ducky"                 = "D:\mr_robot\pico_ducky\setup_pico_ducky.ps1"
     
            }
            #https://ss64.com/ps/
