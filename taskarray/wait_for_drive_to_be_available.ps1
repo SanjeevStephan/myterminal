65860d88157a7806de346fbd8d100885b2fae781
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-21 19:45:48 +0530

    renamed or removed

--- taskarray/wait_for_drive_to_be_available.ps1
+++
@@ -1,9 +0,0 @@
-# Wait for F drive to become available
-do {
-    Write-Host "Waiting for F drive to become available..."
-    Start-Sleep -Seconds 10
-} until (Get-PSDrive F -ErrorAction SilentlyContinue)
-
-# Display confirmation message
-Write-Host "F drive is available."
-
