2100ce07d21254fad3a7ad97d6e7b7116ecfb1fb
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-21 20:34:24 +0530

    removed

--- taskarray/safely_remove_pen_drive.ps1
+++
@@ -1,7 +0,0 @@
-# Define the drive letter
-$driveLetter = "F:"
-
-# Eject the drive
-$eject = New-Object -comObject Shell.Application
-$eject.Namespace(17).ParseName("$driveLetter").InvokeVerb("Eject")
-Write-Output "Safely remove pendrive on drive-letter [F:\]"
