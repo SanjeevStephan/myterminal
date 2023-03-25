ded46fcf2d0a6f2a9cf49e767872b71be8eaba8f
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-25 16:19:35 +0530

    profile version 1.0

---
+++ profile/v1/profile_config.ps1
@@ -0,0 +1,5 @@
+$initial_title = "SuperUser"
+
+#$initial_title = "Be The Conqueror"
+$profile_source = "$Home\Documents\PowerShell"
+$profile_backup_path = "D:\terminal\profile"
---
+++ profile/v1/profile_dict.ps1
@@ -0,0 +1,25 @@
+$func_name = @{
+    "alias"                    = "made up short-nickname used in-place of the actual command"
+    "backup"                   = "backup directory $HOME/Documents/PowerShell"
+    "clip <filename|fileurl>"  = "paste the clipboard into the file"
+    "edit <filename>"          = "edit files in powershell_ise"
+    "figlet <text_to_display>" = "display cool figlet ascii text " 
+    "goto <directory_name>"    = "navigate to directory {py|terminal}"
+    "google"                   = "Send Query to the Google using brave browser"
+    "helptab"                  = "Display helpful command table | try->{pls,terminal}"
+ #   "location"                 = "location of the scripts and directory in profile dictionary"
+    "nano <filename>"          = "open files in nano-terminal-editor"
+    "pls"                      = "[pls-cli] default terminal todo script app"
+    "run <script_name>"        = "execute script like {pygame|pymath}"
+    "refresh"                  = "reload the Microsoft.PowerShell.Profile.ps1 script"
+    "TODO"                     = "Open TODO List in Nano Editor"
+    "window"                   = "open file explorer of the current working directory"
+    "lst"                      = "list only the filesnames"
+    "launch <app_name>"        = "launch utilities such as npad={notepad.exe} | snap={snippingtool.exe}"
+    "getfilename <extension>"  = "convert 'string with spaces' -> 'string-with-space.{py,ps,txt}'"
+
+    
+     
+
+
+}
